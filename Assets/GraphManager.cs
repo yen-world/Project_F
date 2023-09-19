f3bc43ec86c9a8db9ae04d837756382ff1dcd99e
기계공학\user <LKM0222>
2023-09-19 13:41:26 +0900

    그래프 그리기 초기작업

---
+++ Assets/GraphManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphManager : MonoBehaviour
+{
+    [SerializeField] RectTransform graphArea;
+    [SerializeField] GameObject dot;
+    float[] graphXPos = new float[20];
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
